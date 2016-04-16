4c5ca86452815378426ce4980faaacdb096d3c28
Doug Graham <dsgraham@gmail.com>
2016-04-16 12:44:37 -0500

    Add board size

--- desktop/src/lando/systems/ld35/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld35/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package lando.systems.ld35.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import lando.systems.ld35.LudumDare35;
+import lando.systems.ld35.utils.Config;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Config.gameWidth;
+		config.height = Config.gameHeight;
 		new LwjglApplication(new LudumDare35(), config);
 	}
 }
