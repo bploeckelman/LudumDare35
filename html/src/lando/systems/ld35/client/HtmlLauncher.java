a7eed2ad562516ee48d574c71c67d8e8dfc7c59b
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-04-16 21:59:57 -0500

    Fix web build size

--- html/src/lando/systems/ld35/client/HtmlLauncher.java
+++ html/src/lando/systems/ld35/client/HtmlLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import lando.systems.ld35.LudumDare35;
+import lando.systems.ld35.utils.Config;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Config.gameWidth, Config.gameHeight);
         }
 
         @Override
