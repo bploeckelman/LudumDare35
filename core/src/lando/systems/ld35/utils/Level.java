5dcb0e40332c602327d534547c195aeb008d4df7
Doug Graham <dsgraham@gmail.com>
2016-04-18 18:33:59 -0500

    New Level

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -6,6 +6,7 @@ public enum Level {
 
     ZERO  ("maps/level0.tmx",       32, 64, new boolean[] {true, false, false, false, false, false}),
     ONE   ("maps/level1.tmx",       32, 64, new boolean[] {true,  true,  true, false, false, false}),
+    ANVILROCKETTEST   ("maps/level-rocket-anvil-medium.tmx",       32, 64, new boolean[] {true,  true,  true, false, false, false}),
     L4x1  ("maps/level-4-1.tmx",    32, 64, new boolean[] {true,  true,  true,  true, false, false}),
     L5x1  ("maps/level-5-1.tmx",    32, 64, new boolean[] {true,  true,  true,  true,  true, false}),
     FOUR  ("maps/level_door_tutorial.tmx",       32, 64, new boolean[] {true,  true,  true,  true,  true,  true}),
