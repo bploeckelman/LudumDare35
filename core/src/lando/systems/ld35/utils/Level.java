971a58f12bcf203b51c55afd5d9abfb735ed1932
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-04-18 04:14:27 -0500

    Add some levels, level4 doesn't quite work yet

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -3,9 +3,11 @@ package lando.systems.ld35.utils;
 import com.badlogic.gdx.math.Vector2;
 
 public enum Level {
-    ZERO("maps/level0.tmx", 32, 64, new boolean[] {true, false, false, false, false, false}),
-    ONE ("maps/level1.tmx", 32, 64, new boolean[] {true, true, true, false, false, false}),
-    TWO ("maps/level2.tmx", 32, 64, new boolean[] {true, true, true, true, true, true});
+    ZERO  ("maps/level0.tmx", 32, 64, new boolean[] {true, false, false, false, false, false}),
+    ONE   ("maps/level1.tmx", 32, 64, new boolean[] {true, true, true, false, false, false}),
+    TWO   ("maps/level2.tmx", 32, 64, new boolean[] {true, true, true, true, false, false}),
+    THREE ("maps/level3.tmx", 32, 64, new boolean[] {true, true, true, true, true, true}),
+    FOUR  ("maps/level4.tmx", 32, 64, new boolean[] {true, true, true, true, true, true});
 
     public String mapName;
     public float startX;
