eed89e4e54058e0f133e786975c482972b0238b2
Ian McNamara <ian.mcnamara@doit.wisc.edu>
2016-04-18 15:58:51 -0500

    work on levels

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -3,11 +3,13 @@ package lando.systems.ld35.utils;
 import com.badlogic.gdx.math.Vector2;
 
 public enum Level {
-    ZERO  ("maps/level0.tmx", 32, 64, new boolean[] {true, false, false, false, false, false}),
-    ONE   ("maps/level1.tmx", 32, 64, new boolean[] {true, true, true, false, false, false}),
-    TWO   ("maps/level2.tmx", 32, 64, new boolean[] {true, true, true, true, false, false}),
-    THREE ("maps/level3.tmx", 32, 64, new boolean[] {true, true, true, true, true, true}),
-    FOUR  ("maps/level4.tmx", 32, 64, new boolean[] {true, true, true, true, true, true});
+
+    ZERO  ("maps/level0.tmx",       32, 64, new boolean[] {true, false, false, false, false, false}),
+    ONE   ("maps/level1.tmx",       32, 64, new boolean[] {true,  true,  true, false, false, false}),
+    L4x1  ("maps/level-4-1.tmx",    32, 64, new boolean[] {true,  true,  true,  true, false, false}),
+    L5x1  ("maps/level-5-1.tmx",    32, 64, new boolean[] {true,  true,  true,  true,  true, false}),
+    FOUR  ("maps/level3.tmx",       32, 64, new boolean[] {true,  true,  true,  true,  true,  true}),
+    FIVE  ("maps/level4.tmx",       32, 64, new boolean[] {true,  true,  true,  true,  true,  true});
 
     public String mapName;
     public float startX;
@@ -24,4 +26,5 @@ public enum Level {
     public Vector2 getStart() {
         return new Vector2(startX, startY);
     }
+
 }
