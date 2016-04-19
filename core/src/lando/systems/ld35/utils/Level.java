eac762b575a1d7c89bf95384ed116ce3e42e37d0
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-04-18 20:02:26 -0500

    Fixes for challenge level

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -4,17 +4,17 @@ import com.badlogic.gdx.math.Vector2;
 
 public enum Level {
 
-    CHALLENGE_1      ("maps/level-challenge-1.tmx", 32, 64,         new boolean[] {true,  true,  true,  true,  true,  true}),
     INTRODUCTION     ("maps/level-intro.tmx", 32, 64,               new boolean[] {true, false, false, false, false, false}),
     ROCKETANVIL_EASY ("maps/level-rocket-anvil-easy.tmx", 32, 64,   new boolean[] {true,  true,  true, false, false, false}),
     ROCKETANVIL_MED  ("maps/level-rocket-anvil-medium.tmx", 32, 64, new boolean[] {true,  true,  true, false, false, false}),
     TORUS_TUTORIAL   ("maps/level-torus-tutorial.tmx", 32, 64,      new boolean[] {true,  true,  true,  true, false, false}),
     TORUS_MED        ("maps/level-torus-medium.tmx", 32, 64,        new boolean[] {true,  true,  true,  true, false, false}),
     MAGNET_EASY      ("maps/level-magnet-easy.tmx", 32, 64,         new boolean[] {true,  true,  true,  true,  true, false}),
-    MAGNET_TORUS_HARD("maps/level-magnet-torus-hard.tmx", 32, 64,   new boolean[] {true,  true,  true,  true,  true, false}),
     ROPE_TUTORIAL    ("maps/level-rope-tutorial.tmx", 32, 64,       new boolean[] {true,  true,  true,  true,  true,  true}),
     DOOR_TUTORIAL    ("maps/level_door_tutorial.tmx", 32, 64,       new boolean[] {true,  true,  true,  true,  true,  true}),
     DOOR_MED         ("maps/level-branching-doors.tmx", 32, 64,     new boolean[] {true,  true,  true,  true,  true,  true}),
+    MAGNET_TORUS_HARD("maps/level-magnet-torus-hard.tmx", 32, 64,   new boolean[] {true,  true,  true,  true,  true,  true}),
+    CHALLENGE_1      ("maps/level-challenge-1.tmx", 32, 64,         new boolean[] {true,  true,  true,  true,  true,  true}),
     SPIKES_HARD      ("maps/level-spikes-hard.tmx", 32, 64,         new boolean[] {true,  true,  true,  true,  true,  true});
 
     public String mapName;
