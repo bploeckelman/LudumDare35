2e347ea505784ca899491bbf878781e0c9f1613f
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2017-01-26 21:16:05 -0600

    Swap out end level

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -69,9 +69,9 @@ public enum Level {
     TRICKY_DROPS                     ("maps/Tricky_Drops.tmx",                     32, 64, new boolean[] { true, true, true, true, true, true}),
     TROY_IS_SUPER_PROUD_OF_THIS_ONE  ("maps/Troy_Is_Super_Proud_of_This_One.tmx",  32, 64, new boolean[] { true, true, true, true, true, true}),
     UNNECESSARY_ROPES_               ("maps/Unnecessary_Ropes.tmx",                32, 64, new boolean[] { true, true, true, true, true, true}),
-    UP_AROUND_AND_BACK_AGAIN         ("maps/Up_Around_and_Back_Again.tmx",         32, 64, new boolean[] { true, true, true, true, true, true});
+    UP_AROUND_AND_BACK_AGAIN         ("maps/Up_Around_and_Back_Again.tmx",         32, 64, new boolean[] { true, true, true, true, true, true}),
 //    WIDE_PATHWAYS_AND_LONG_ROPES     ("maps/Wide_Pathways_and_Long_Ropes.tmx",     32, 64, new boolean[] { true, true, true, true, true, true}),
-//    END_CREDITS                      ("maps/End_Credits_unfinished_concept.tmx",   32, 64, new boolean[] { true, true, true, true, true, true});
+    END_CREDITS                      ("maps/End_Credits.tmx",   32, 64, new boolean[] { true, true, true, true, true, true});
 
 
     public String mapName;
