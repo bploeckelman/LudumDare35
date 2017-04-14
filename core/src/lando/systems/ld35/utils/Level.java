c81c451b2291dc78a094a1526b8837747e0980eb
Troy Sullivan <troysulli@hotmail.com>
2017-04-13 21:12:37 -0500

    Moved Gates_Everywhere

--- core/src/lando/systems/ld35/utils/Level.java
+++ core/src/lando/systems/ld35/utils/Level.java
@@ -31,7 +31,7 @@ public enum Level {
 
     DOWN_BELOW_THE_RECTANGLES        ("maps/Down_Below_The_Rectangles.tmx",        32, 64, new boolean[] { true, true, true, true, true, true}),
     EASY_PEEZY                       ("maps/Easy_Peezy.tmx",                       32, 64, new boolean[] { true, true, true, true, true, true}),
-    GATES_EVERYWHERE                 ("maps/Gates_Everywhere.tmx",                 32, 64, new boolean[] { true, true, true, true, true, true}),
+
     FIGHT_THE_CURRENT                ("maps/Fight_The_Current.tmx",                32, 64, new boolean[] { true, true, true, true, true, true}),
     FIGHT_THE_CURRENT_2              ("maps/Fight_The_Current_2.tmx",              32, 64, new boolean[] { true, true, true, true, true, true}),
     FIGHT_THE_CURRENT_3              ("maps/Fight_The_Current_3.tmx",              32, 64, new boolean[] { true, true, true, true, true, true}),
@@ -76,7 +76,7 @@ public enum Level {
 
     TOP_OR_BOTTOM_PATH               ("maps/Top_or_Bottom_Path.tmx",               32, 64, new boolean[] { true, true, true, true, true, true}),
     TOTALLY_NECESSARY_ROPES          ("maps/Totally_Necessary_Ropes.tmx",          32, 64, new boolean[] { true, true, true, true, true, true}),
-
+    GATES_EVERYWHERE                 ("maps/Gates_Everywhere.tmx",                 32, 64, new boolean[] { true, true, true, true, true, true}),
 
     TROY_IS_SUPER_PROUD_OF_THIS_ONE  ("maps/Troy_Is_Super_Proud_of_This_One.tmx",  32, 64, new boolean[] { true, true, true, true, true, true}),
     UNNECESSARY_ROPES_               ("maps/Unnecessary_Ropes.tmx",                32, 64, new boolean[] { true, true, true, true, true, true}),
